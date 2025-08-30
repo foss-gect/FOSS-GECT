685b42d85ed181f77f57d9306203dc2e0da17934
A0D1I2L3 <adilhaneefa2005@gmail.com>
2025-08-30 22:10:30 +0530

    readme

--- app/page.tsx
+++ app/page.tsx
@@ -47,3 +47,30 @@ const Navbar = () => {
 };
 
 
+
+export default function Home() {
+  return (
+    <div>
+    <div className="relative min-h-screen bg-black cabinet-font">
+      <Navbar />
+      <WarpBackground
+        className="absolute inset-0 z-0 landing-page-bg"
+        gridColor="transparent"
+        beamSize={15}
+        beamsPerSide={10}
+        perspective={2400}
+        beamDelayMin={0}
+        beamDelayMax={4}
+        beamDuration={3}
+      >
+        <div className="w-full h-full relative">
+          <div className="absolute inset-0 bg-transparent"></div>
+          <div className="absolute top-1/4 left-1/4 w-1/2 h-1/2 bg-black z-10 pointer-events-none"></div>
+        </div>
+      </WarpBackground>
+
+        
+      </div>
+    </div>
+  );
+}
